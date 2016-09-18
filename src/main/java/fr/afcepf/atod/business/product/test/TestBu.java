c1b4837dc479cd13cf1483f5ec5be397fbf439b6
ronan <ronan@RL-Satellite-P850>
2016-09-18 14:56:56 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/fr/afcepf/atod/business/product/api/IBuProduct.java

--- src/main/java/fr/afcepf/atod/business/product/test/TestBu.java
+++ src/main/java/fr/afcepf/atod/business/product/test/TestBu.java
@@ -1,49 +1,60 @@
 package fr.afcepf.atod.business.product.test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import fr.afcepf.atod.business.product.api.IBuCity;
 import fr.afcepf.atod.business.product.api.IBuProduct;
 import fr.afcepf.atod.vin.data.exception.WineException;
-import fr.afcepf.atod.wine.entity.City;
 import fr.afcepf.atod.wine.entity.ProductType;
 import fr.afcepf.atod.wine.entity.ProductVarietal;
+import fr.afcepf.atod.wine.entity.ProductVintage;
 import fr.afcepf.atod.wine.entity.ProductWine;
 import java.util.Map;
 
 public class TestBu {
-	private static Logger log = Logger.getLogger(TestBu.class);
 
-	public static void main(String[] args) throws WineException {
-		@SuppressWarnings("resource")
-		BeanFactory bf = new ClassPathXmlApplicationContext("classpath:springBuProduct.xml");
-		
-                
-                IBuProduct buProductbis  = (IBuProduct) 
-                        bf.getBean(IBuProduct.class);
-                
-                List<ProductType> wineTypes = buProductbis.getWineTypes();
-                Map<ProductType, List<ProductVarietal>> varietals 
-                        = buProductbis.getVarietalsByType(wineTypes);
-                List<ProductWine> wines = null;
-                
-                for(Map.Entry<ProductType,List<ProductVarietal>> 
+    private static Logger log = Logger.getLogger(TestBu.class);
+
+    public static void main(String[] args) throws WineException {
+        @SuppressWarnings("resource")
+        BeanFactory bf = new ClassPathXmlApplicationContext("classpath:springBuProduct.xml");
+
+        IBuProduct buProductbis = (IBuProduct) bf.getBean(IBuProduct.class);
+
+        List<ProductType> wineTypes = buProductbis.getWineTypes();
+        List<ProductVintage> vintages = buProductbis.findProductsByVintage(2010);
+
+        Map<ProductType, List<ProductVarietal>> varietals
+                = buProductbis.getVarietalsByType(wineTypes);
+        List<ProductWine> wines = null;
+
+        /*for(Map.Entry<ProductType,List<ProductVarietal>> 
                         e : varietals.entrySet()){
                     List<ProductVarietal> listVarietal = e.getValue();
-                    for(ProductVarietal prod : listVarietal) {
+                    /*for(ProductVarietal prod : listVarietal) {
                         wines = buProductbis.categoryAccordingToObjectType(e.getKey(), 
                                 50);
                         for (ProductWine wine : wines) {
                             log.info("\t # wine: " + wine.getDescription());
                         }
-                    }
+                    }   
+                }*/
+        for (ProductVintage vintage : vintages) {
+            for (ProductType type : wineTypes) {
+                try {
+                    wines = buProductbis.categoryAccordingToObjectType(type, vintage);
+                } catch (Exception e) {
+
                 }
-                
-	}
+                for (ProductWine wine : wines) {
+                    log.info("\t # wine: " + wine.getDescription());
+                }
+            }
+        }
+
+    }
 
 }
